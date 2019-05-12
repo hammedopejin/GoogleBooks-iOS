33d3dd7b986affcc382759705d9de832382ca5b1
Hammed Opejin <hammedopejin@yahoo.com>
2019-05-11 23:25:28 -0400

    feat: Create FavoritesViewController

---
+++ GoogleBooks/ViewController/FavoritesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FavoritesViewController.swift
+//  GoogleBooks
+//
+//  Created by Hammed opejin on 5/11/19.
+//  Copyright © 2019 Hammed opejin. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+    
+    @IBOutlet weak var favoritesTableview: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
